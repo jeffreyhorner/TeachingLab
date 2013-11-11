06766c1033c92c63cbf4820effb28b1d5cf1905a
jeffreyhorner <jeffrey.horner@gmail.com>
2013-11-11 13:03:58 -0600

    Get rid of tab code

--- server.R
+++ server.R
@@ -5,14 +5,6 @@ options(shiny.trace=TRUE)
 source('shinyExtras.R')
 
 shinyServer(function(input, output) {
-  output$tab1 <- reactiveText(function(){
-    'Tab 1 Output'
-  })
-
-  output$tab2 <- reactiveText(function(){
-    'Tab 2 Output'
-  })
-
   # reactiveRmd returns a function that will return the output of knit2html
   # on the Rmd file. Additionally it will only re-run knit2html if the Rmd file
   # has changed.
